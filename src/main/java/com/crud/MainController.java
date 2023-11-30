383e832db6026aed26210340ff4d9c283712c282
Selma-ait <113130101+Selma-ait@users.noreply.github.com>
2023-11-30 21:53:45 +0100

    Add home, login, register pages

--- src/main/java/com/crud/MainController.java
+++ src/main/java/com/crud/MainController.java
@@ -9,6 +9,16 @@ public class MainController {
     @GetMapping("")
     public String showHomePage(){
 //        System.out.println("MainController");
-        return "index";
+        return "Home";
     }
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // Assurez-vous d'avoir une vue ou un mapping approprié pour la page de connexion
+    }
+    @GetMapping("/register")
+    public String register() {
+        return "register"; // Assurez-vous d'avoir une vue ou un mapping approprié pour la page de connexion
+    }
+
+
 }
